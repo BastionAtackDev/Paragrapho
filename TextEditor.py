124f022039cf94c592df6dc735f1e7da19721119
BastionAtackDev <88817320+BastionAtackJimmy@users.noreply.github.com>
2022-09-04 12:00:56 +0300

    Add files via upload

--- TextEditor.py
+++ TextEditor.py
@@ -1,14 +1,30 @@
 from tkinter import *
-from sys import *
+from tkinter import filedialog
+
+#from sys import *
 
 def submit():
+    file = filedialog.asksaveasfile(defaultextension='.txt',
+                                    filetypes=[
+                                        ("All files",".*")
+                                    ])
     input = text.get('1.0',END)
-    print(input)
+    file.write(input)
+    file.close()
+
+def getFile():
+    path = filedialog.askopenfilename()
+    file = open(path,'rt')
+    text.insert('1.0',file.read())
+    file.close()
 root = Tk()
+root.title("Text Editor")
+save = Button(root,text='SAVE',command=submit)
+save.pack(side="top")
+openbutton = Button(root,text="Open",command=getFile)
+openbutton.pack()
 text = Text(root)
 text.pack()
-with open('test.txt') as file:
-    text.insert('1.0',file)
-button = Button(root,text='SAVE',command=submit)
-button.pack()
+#with open('test.txt') as file:
+#    text.insert('1.0',file)
 root.mainloop()
